d8fc509ec9ab722c2463a80adcd1651d8ae93549
axumrs <rolson867@gmail.com>
2023-05-16 14:29:23 +0800

    标签添加

--- src/pages/Tag/Add.jsx
+++ src/pages/Tag/Add.jsx
@@ -1,5 +1,29 @@
+import { ProForm, ProFormText } from "@ant-design/pro-components";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import fetcher from "../../fetcher";
 
 export default function TagAdd() {
-  return <div>TagAdd</div>;
+  const navigate = useNavigate();
+  const sumbitHandler = async (values) => {
+    const { data: res } = await fetcher.post("/tag", values);
+    if (res.code == 0) {
+      navigate("/tag/list");
+    }
+  };
+  return (
+    <>
+      <ProForm onFinish={sumbitHandler}>
+        <ProFormText
+          name="name"
+          label="标签名称"
+          placeholder="请输入标签名称"
+          rules={[
+            { required: true, message: "请输入标签名称" },
+            { max: 100, message: "标签名称超过长度" },
+          ]}
+        />
+      </ProForm>
+    </>
+  );
 }
