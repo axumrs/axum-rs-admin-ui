08cc07d720fe5073eaace0c22a0c5b3ae501f220
axumrs <rolson867@gmail.com>
2024-10-10 11:09:52 +0800

    布局

---
+++ components/PageTitle.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ icon?: string }>();
+</script>
+
+<template>
+  <h1 class="text-lg flex justify-start items-center gap-x-1">
+    <Icon :name="icon" v-if="icon" />
+    <slot></slot>
+  </h1>
+</template>
